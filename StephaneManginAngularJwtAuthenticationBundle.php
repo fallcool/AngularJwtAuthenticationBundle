58c4221362838a8ae390b3a137ed766d77e67c8d
Stéphane Mangin <StephaneMangin@users.noreply.github.com>
2015-04-05 12:31:17 +0200

    Update and rename AngularJwtAuthenticationBundle.php to StephaneManginAngularJwtAuthenticationBundle.php

--- StephaneManginAngularJwtAuthenticationBundle.php
+++ StephaneManginAngularJwtAuthenticationBundle.php
@@ -4,6 +4,6 @@ namespace StephaneMangin\Bundle\AngularJwtAuthentication;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AngularJwtAuthenticationBundle extends Bundle
+class StephaneManginAngularJwtAuthenticationBundle extends Bundle
 {
 }
