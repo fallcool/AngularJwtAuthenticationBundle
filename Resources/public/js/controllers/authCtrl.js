79ae2fb0e2c1e00043aa746bdf0de0ee5dbbd2b8
Stéphane Mangin <StephaneMangin@users.noreply.github.com>
2015-04-10 18:54:15 +0200

    Update authCtrl.js

--- Resources/public/js/controllers/authCtrl.js
+++ Resources/public/js/controllers/authCtrl.js
@@ -5,28 +5,24 @@
 	/**
 	 * Controller for client route
 	 */
-	angular
-		.module('authJwt')
+	angular.module('authJwt')
 		.controller('AuthCtrl', function($rootScope, $route, $scope, $routeParams, $http, $location, $modal, localStorageService, AuthenticationService) {
-
 			$scope.connected = false;
 			if(localStorageService.get('token') !== null) {
-        		$http.defaults.headers.common.Authorization = 'Bearer ' + localStorageService.get('token');
-                $rootScope.$broadcast('event:auth-login-complete');
-                console.log($http.defaults.headers.common.Authorization);
+        			$http.defaults.headers.common.Authorization = 'Bearer ' + localStorageService.get('token');
+                		$rootScope.$broadcast('event:auth-login-complete');
 				$scope.connected = true;
-        	}
-			
-            $scope.$on('event:auth-loginRequired', function () {
-            	if(!localStorageService.get('token')) {
-            		$modal.open({
-	                    templateUrl: '/bundles/stephanemanginangularjwtauthentication/templates/login.html',
-	                    controller:  'LoginModalCtrl',
-	                    backdrop:    'static'
-	                });
+        		}
+            		$scope.$on('event:auth-loginRequired', function () {
+	            		if(!localStorageService.get('token')) {
+	            			$modal.open({
+	            				templateUrl: '/bundles/stephanemanginangularjwtauthentication/templates/login.html',
+		                    		controller:  'LoginModalCtrl',
+		                    		backdrop:    'static'
+		                	});
 					$scope.connected = true;
-            	}
-            });
+	            		}
+	            	});
 			$scope.login = function() {
 				$rootScope.$broadcast('event:auth-loginRequired');
 			}
@@ -36,6 +32,5 @@
 				$location.path('/');
 				$route.reload();
 			}
-            
 		});
 })();
