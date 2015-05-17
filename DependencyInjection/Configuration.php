cb9b8095e05ea2a12c5d6724e20269f32e29ec0c
Stéphane Mangin <StephaneMangin@users.noreply.github.com>
2015-05-17 16:55:58 +0200

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('axeria_auth_jwt');
+        $rootNode = $treeBuilder->root('auth_jwt');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
